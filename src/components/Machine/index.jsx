1e91757cfd9745cf90b9c229748e1f5c74b72a59
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-23 11:19:22 -0700

    play audio onclick or keydown

--- src/components/Machine/index.jsx
+++ src/components/Machine/index.jsx
@@ -3,17 +3,42 @@ import Pad from "../Pad";
 import styles from "./styles.module.scss";
 
 const PAD_ARR = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
-function Machine() {
-  const [key, setKey] = useState("");
+const PAD_DATA = [
+  { id: "Q", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+  { id: "W", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+  { id: "E", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+  {
+    id: "A",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+  { id: "S", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
+  { id: "D", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  {
+    id: "Z",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+  {
+    id: "X",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+  { id: "C", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
+];
 
-  const handlePress = (value) => {
+function Machine() {
+  const handlePress = (value, url) => {
     console.log("pressed", value);
+    const a = new Audio(url);
+    a.play();
   };
 
   useEffect(() => {
     const keydownListener = (e) => {
       if (PAD_ARR.includes(e.key.toUpperCase())) {
-        handlePress(e.key);
+        console.log(PAD_DATA.find((x) => x.id === e.key.toUpperCase()).url);
+        handlePress(
+          e.key,
+          PAD_DATA.find((x) => x.id === e.key.toUpperCase()).url
+        );
       }
     };
     window.addEventListener("keydown", keydownListener);
@@ -26,14 +51,14 @@ function Machine() {
     <div>
       <h2>Machine</h2>
       <ul className={styles.padList}>
-        {PAD_ARR
-          ? PAD_ARR.map((elem, i) => {
+        {PAD_DATA
+          ? PAD_DATA.map((obj, i) => {
               const onClick = () => {
-                handlePress(elem);
+                handlePress(obj.id, obj.url);
               };
               return (
                 <li key={i}>
-                  <Pad onClick={onClick} label={elem} />
+                  <Pad onClick={onClick} label={obj.id} />
                 </li>
               );
             })
