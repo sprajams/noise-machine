eccea0c07ea8ff35b597fce8245e1cb9d6284229
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-23 11:37:17 -0700

    clean up, and label added to each audio obj

--- src/components/Machine/index.jsx
+++ src/components/Machine/index.jsx
@@ -4,24 +4,51 @@ import styles from "./styles.module.scss";
 
 const PAD_ARR = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
 const PAD_DATA = [
-  { id: "Q", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
-  { id: "W", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
-  { id: "E", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" },
+  {
+    id: "Q",
+    label: "Heater 1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
+  {
+    id: "W",
+    label: "Heater 2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    id: "E",
+    label: "Heater 3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
   {
     id: "A",
+    label: "Heater 4",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
   },
-  { id: "S", url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" },
-  { id: "D", url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" },
+  {
+    id: "S",
+    label: "Heater 6",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+  {
+    id: "D",
+    label: "Open HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
   {
     id: "Z",
+    label: "Kick n Hat",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
     id: "X",
+    label: "Kick 1",
     url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
   },
-  { id: "C", url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
+  {
+    id: "C",
+    label: "Close HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
 ];
 
 function Machine() {
