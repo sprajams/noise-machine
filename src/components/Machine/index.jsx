1650174ab5a24675ee6f63bc88d6c7270c8df2a3
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-25 16:19:56 -0700

    pad handles events

--- src/components/Machine/index.jsx
+++ src/components/Machine/index.jsx
@@ -3,50 +3,49 @@ import Pad from "../Pad";
 import Display from "../Display";
 import styles from "./styles.module.scss";
 
-const PAD_ARR = ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"];
 const PAD_DATA = [
   {
-    id: "Q",
+    id: "q",
     label: "Heater 1",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
   },
   {
-    id: "W",
+    id: "w",
     label: "Heater 2",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
   },
   {
-    id: "E",
+    id: "e",
     label: "Heater 3",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
   },
   {
-    id: "A",
+    id: "a",
     label: "Heater 4",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
   },
   {
-    id: "S",
+    id: "s",
     label: "Heater 6",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
   },
   {
-    id: "D",
+    id: "d",
     label: "Open HH",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
   },
   {
-    id: "Z",
+    id: "z",
     label: "Kick n Hat",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
   },
   {
-    id: "X",
+    id: "x",
     label: "Kick 1",
     url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
   },
   {
-    id: "C",
+    id: "c",
     label: "Close HH",
     url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
   },
@@ -68,27 +67,11 @@ function Machine() {
     a.volume = volume / 100;
   };
 
-  useEffect(() => {
-    if (isOn) {
-      const keydownListener = (e) => {
-        if (PAD_ARR.includes(e.key.toUpperCase())) {
-          const targetObj = PAD_DATA.find((x) => x.id === e.key.toUpperCase());
-          handlePress(targetObj, targetObj.url);
-        }
-      };
-      window.addEventListener("keydown", keydownListener);
-      return () => {
-        window.removeEventListener("keydown", keydownListener);
-      };
-    } else {
-      setLabel("");
-    }
-  }, [isOn]);
-
   return (
     <div>
-      <div className={styles.main}>
+      <div className={styles.main} id="drum-machine">
         <Display
+          id="display"
           label={label}
           setOn={setIsOn}
           on={isOn}
@@ -104,8 +87,8 @@ function Machine() {
                   }
                 };
                 return (
-                  <li key={i}>
-                    <Pad onClick={onClick} label={obj.id} />
+                  <li key={i} className="drum-pad" id={i}>
+                    <Pad id={obj.id} label={obj.label} audioSrc={obj.url} />
                   </li>
                 );
               })
--- src/components/Pad/index.jsx
+++ src/components/Pad/index.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useRef, useState } from "react";
+import clsx from "clsx";
 import styles from "./styles.module.scss";
 
-function Pad({ label, onClick }) {
+function Pad({ id, audioSrc, label }) {
+  const [keyPressed, setKeyPressed] = useState(false);
+  const audioRef = useRef(null);
+
+  const playSound = () => {
+    audioRef.current.play();
+  };
+
+  useEffect(() => {
+    const keydownListener = (e) => {
+      if (e.key === id) {
+        playSound();
+        setKeyPressed(true);
+      }
+    };
+    const keyupListener = (e) => {
+      if (e.key === id) {
+        setKeyPressed(false);
+      }
+    };
+    window.addEventListener("keydown", keydownListener);
+    window.addEventListener("keyup", keyupListener);
+    return () => {
+      window.removeEventListener("keydown", keydownListener);
+      window.removeEventListener("keyup", keyupListener);
+    };
+  }, [id]);
+
   return (
     <div>
-      <button onClick={onClick} className={styles.btn}>
-        <span className={styles.btnTop}>{label}</span>
+      <button
+        onClick={playSound}
+        className={clsx(styles.btn, keyPressed && styles.keyPressed)}
+        aria-label={label}
+      >
+        <span className={styles.btnTop}>{id.toUpperCase()}</span>
         <span className={styles.btnBottom}></span>
       </button>
+      <audio ref={audioRef} src={audioSrc}></audio>
     </div>
   );
 }
